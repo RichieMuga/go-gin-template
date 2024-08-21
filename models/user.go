b160af968719d352bcbc454786b6bc70d6898928
Richie Mugambi <richiemugambi4@gmail.com>
2024-08-21 22:14:46 +0300

    deps: add gorm

---
+++ models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Email       string    `gorm:"type:varchar(100);uniqueIndex;not null"`
+	FirstName   string
+	SecondName  string
+	PhoneNumber string
+	Password    string
+	IsActive    bool `gorm:"default:false"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
