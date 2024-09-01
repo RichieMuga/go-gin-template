966d00cd4fd2f8f734adeaa18094d3268454d1ea
Richie Mugambi <richiemugambi4@gmail.com>
2024-09-01 15:09:31 +0300

    feat: add swagger api auto generation configaration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,41 @@
 package main
 
 import (
-	"github.com/RichieMuga/go-gin-template/database"
-	"github.com/RichieMuga/go-gin-template/pkg/logger"
-
-	"github.com/gin-gonic/gin"
+    "github.com/RichieMuga/go-gin-template/database"
+    docs "github.com/RichieMuga/go-gin-template/docs"
+    "github.com/RichieMuga/go-gin-template/pkg/logger"
+    "github.com/RichieMuga/go-gin-template/routes"
+    "github.com/gin-gonic/gin"
+    swaggerfiles "github.com/swaggo/files"
+    ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+//	@title			Gin Swagger Go-gin-template
+//	@version		1.0
+//	@description	This is a sample server for a Gin application.
+//	@host			localhost:8080
+//	@BasePath		/api/v1
+
 func main() {
-  // Initialize the logger
-  logger.InitLogger()
-  
+	// Initialize the logger
+	logger.InitLogger()
+
 	// Initialize router
-  router := gin.Default()
+	router := gin.Default()
 
-	// Test api
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-    logger.Info("ping request sent")
-	})
+	// Initialize routes
+	routes.InitializeRoutes(router)
 
-  // Connect database
-  database.ConnectDatabase()
+	// Connect database
+	database.ConnectDatabase()
 
-  // Start the server and check for errors
-  router.Run()
+	// Set the Swagger base path to match your API version
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
+	// Initialize swagger docs
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler, url))
+
+	// Start the server and check for errors
+	router.Run()
 }
